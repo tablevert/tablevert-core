fc0329243428974c68f5d3b7ca28aa555db1ec68
doc-hil <doc-hil@conis.de>
2019-03-13 16:37:57 +0100

    Added slf4j

--- src/main/java/org/tablevert/core/DefaultTableverter.java
+++ src/main/java/org/tablevert/core/DefaultTableverter.java
@@ -5,12 +5,22 @@ SPDX-License-Identifier: Apache-2.0
 
 package org.tablevert.core;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Default implementation of the {@link org.tablevert.core.Tableverter} interface.
  *
  * @author doc-hil
  */
 public class DefaultTableverter implements Tableverter {
+    final Logger logger = LoggerFactory.getLogger(DefaultTableverter.class);
+
+    public DefaultTableverter() {
+        logger.info("Set up DefaultTableverter");
+
+    }
+
     public String getDummyDummyString() {
         return "Ain't this a dummy string?";
     }
