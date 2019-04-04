7ac68d9111185709d0775b3b1b1dd9154b4b8216
doc-hil <doc-hil@conis.de>
2019-04-04 15:25:46 +0200

    Added validation to SimpleTablevertConfig.Builder

--- src/main/java/org/tablevert/core/config/SimpleTablevertConfig.java
+++ src/main/java/org/tablevert/core/config/SimpleTablevertConfig.java
@@ -5,6 +5,8 @@
 
 package org.tablevert.core.config;
 
+import org.tablevert.core.BuilderFailedException;
+
 import javax.xml.crypto.Data;
 import java.util.Hashtable;
 import java.util.List;
@@ -24,17 +26,32 @@ public class SimpleTablevertConfig implements TablevertConfig, Cloneable {
         private final Map<String, DatabaseQuery> queryMap;
         private Map<String, DatabaseUser> userMap;
 
+        /**
+         * Instantiates the builder.
+         */
         public Builder() {
             this.queryMap = new Hashtable<>();
             this.databaseMap = new Hashtable<>();
             this.userMap = new Hashtable<>();
         }
 
+        /**
+         * Adds a {@link Database} instance to the builder.
+         * If a database with the same name already exists, it is replaced.
+         * @param database the database to add
+         * @return the builder
+         */
         public Builder withDatabase(Database database) {
             putOrReplace(databaseMap, database.getName(), database);
             return this;
         }
 
+        /**
+         * Adds multiple {@link Database} instances to the builder.
+         * Calls {@code withDatabase} for each database.
+         * @param databases the databases to add
+         * @return the builder
+         */
         public Builder withDatabases(List<Database> databases) {
             if (databases != null) {
                 for (Database database : databases) {
@@ -44,11 +61,23 @@ public class SimpleTablevertConfig implements TablevertConfig, Cloneable {
             return this;
         }
 
+        /**
+         * Adds a {@link DatabaseQuery} instance to the builder.
+         * If a query with the same name already exists, it is replaced.
+         * @param query the query to add
+         * @return the builder
+         */
         public Builder withQuery(DatabaseQuery query) {
             putOrReplace(queryMap, query.getName(), query);
             return this;
         }
 
+        /**
+         * Adds multiple {@link DatabaseQuery} instances to the builder.
+         * Calls {@code withQuery} for each query.
+         * @param queries the queries to add
+         * @return the builder
+         */
         public Builder withQueries(List<DatabaseQuery> queries) {
             if (queries != null) {
                 for (DatabaseQuery query : queries) {
@@ -58,11 +87,46 @@ public class SimpleTablevertConfig implements TablevertConfig, Cloneable {
             return this;
         }
 
-        public SimpleTablevertConfig build() {
+        /**
+         * Creates a {@link SimpleTablevertConfig} object from the parameters passed to the builder.
+         * @return the built configuration object
+         */
+        public SimpleTablevertConfig build()throws BuilderFailedException {
+            validate();
             SimpleTablevertConfig config = new SimpleTablevertConfig();
             config.queryMap = this.queryMap;
             config.databaseMap = this.databaseMap;
             return config;
+        }
+
+        private void validate() throws BuilderFailedException {
+            String errors = "";
+            if (!queryMap.isEmpty()) {
+                errors += detectQueryMapErrors();
+            }
+            if (!errors.isEmpty()) {
+                throw new BuilderFailedException("Builder validation failed with errors: " + errors);
+            }
+        }
+
+        private String detectQueryMapErrors() {
+            String errors = "";
+            for (Map.Entry<String, DatabaseQuery> mapEntry : queryMap.entrySet()) {
+                errors += detectQueryErrors(mapEntry.getValue());
+            }
+            return errors;
+        }
+
+        private String detectQueryErrors(DatabaseQuery query) {
+            if (query == null) {
+                return  "Query is not defined; ";
+            }
+            String errors = "";
+            if (databaseMap.get(query.getDatabaseName()) == null) {
+                errors += "Database [" + query.getDatabaseName() + "] associated with query ["
+                        + query.getName() + "] is not configured; ";
+            }
+            return errors;
         }
 
         private void putOrReplace(Map map, String key, Object value) {
